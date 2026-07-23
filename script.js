46e3b5cd543bdafb9d258ffeba43e4ec38089107
shrirammandeer-creator <shrirammandeer@gmail.com>
2026-07-23 12:44:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-console.log("Step 3 Loaded ❤️");
+console.log("Website Loaded ❤️");
 
+// Loading → Welcome
 setTimeout(() => {
 
     document.getElementById("loadingScreen").classList.remove("active");
-
     document.getElementById("welcomeScreen").classList.add("active");
 
-},3000);
+}, 3000);
 
-document.getElementById("continueBtn").addEventListener("click",()=>{
+
+// Welcome → Intro
+document.getElementById("startBtn").addEventListener("click", () => {
+
+    document.getElementById("welcomeScreen").classList.remove("active");
+    document.getElementById("introScreen").classList.add("active");
+
+});
+
+
+// Intro → Quiz
+document.getElementById("continueBtn").addEventListener("click", () => {
 
     document.getElementById("introScreen").classList.remove("active");
-
     document.getElementById("quizScreen").classList.add("active");
 
 });

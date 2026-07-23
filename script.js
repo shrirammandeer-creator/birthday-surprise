da4156f9b3e85a18ebeba298932ba9a55c083df7
shrirammandeer-creator <shrirammandeer@gmail.com>
2026-07-23 11:55:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Loading Screen Ready ❤️");
+console.log("Step 3 Loaded ❤️");
+
+setTimeout(() => {
+
+    document.getElementById("loadingScreen").classList.remove("active");
+
+    document.getElementById("welcomeScreen").classList.add("active");
+
+},3000);
+
+document.getElementById("startBtn").addEventListener("click",()=>{
+
+    alert("Perfect! The next step will open the Romantic Quiz ❤️");
+
+});
